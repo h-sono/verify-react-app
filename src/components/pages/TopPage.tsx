22e5ca6d9276594043e11c81ba1a1f7e34a08696
root <root@DESKTOP-M5NNB27>
2024-02-07 17:31:11 +0900

    [update]:.gitignore更新。

--- src/components/pages/TopPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { TopTable } from '../organisms/TopTable.tsx';
-
-export const TopPage: React.FC = () => {
-  return <TopTable memo='テストメモ' date='2024/1/22' button='テストボタン' />;
-};
